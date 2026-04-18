22090f95f54f389ed834c290aaa50ce2d07d7197
meglehetosen <sasvaribalint13@gmail.com>
2026-04-18 20:24:14 +0200

    Validate password fuggveny es sikeres teszteles

--- UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/Controllers/AccountController.cs
@@ -50,7 +50,18 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
+            if (password.Length < 8)
+                return false;
+            if(string.IsNullOrEmpty(password))
+                return false;
+            if (!Regex.IsMatch(password, @"[0-9]"))
+                return false;
+            if (!Regex.IsMatch(password, @"[a-z]"))
+                return false;
+            if (!Regex.IsMatch(password, @"[A-Z]"))
+                return false;
             return true;
+
         }
     }
 }
